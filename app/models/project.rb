c5d55b53a7bedfada505171f31047ae58c221abe
caryamiel <amielasparen@gmail.com>
2016-01-15 00:19:18 +0800

    deploying

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
 	has_many :tasks, dependent: :destroy
 	belongs_to :user
+
+
+  	has_many :projectmemberships, dependent: :destroy
+	has_many :members, through: :projectmemberships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,7 @@ has_secure_password
   has_many :friends, :through => :friendships, dependent: :destroy
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id", dependent: :destroy
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user, dependent: :destroy
+
+  has_many :projectmemberships , :dependent :destroy
+  has_many :joinedproject, through: :projectmemberships, source: :user
 end
