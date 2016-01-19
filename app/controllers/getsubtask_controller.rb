08a9ea09d970390483255c1af1af37bbc5aa96cd
caryamiel <amielasparen@gmail.com>
2016-01-20 02:46:30 +0800

    config to push

--- app/controllers/getsubtask_controller.rb
+++
@@ -1,9 +0,0 @@
-class GetsubtaskController < ApplicationController
-  def create
-  		task_id = params [:task_id]
-		@subtask =  Subtask.where(subtask.task_id= task_id)
-		render json: {status: :success, subtask: @subtask}
-	
-  end
-
-end
