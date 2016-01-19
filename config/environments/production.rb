9467740ca2c6cc1274a4b92eb2796db47810633e
caryamiel <amielasparen@gmail.com>
2016-01-19 14:22:43 +0800

    config productions.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
 
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   sending Email
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { :host => "frozen-wave-5827.herokuapp.com" }
+    config.action_mailer.default_url_options = { :host => 'frozen-wave-5827.herokuapp.com' }
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
@@ -96,4 +96,5 @@ Rails.application.configure do
     }
 
     config.action_mailer.perform_deliveries = true
+    config.action_mailer.default :charset => "utf-8"
 end
