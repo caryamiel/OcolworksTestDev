22d397a52f56539219f327d577a14d6ad7b57fd1
caryamiel <amielasparen@gmail.com>
2016-02-04 16:27:31 +0800

    remove migration pmember

--- db/migrate/20160109045258_add_project_confirmation_to_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectConfirmationToProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :project_confirmation, :boolean
-  end
-end
--- db/migrate/20160204080505_add_pmember_id_to_projectmemberships.rb
+++
@@ -1,5 +0,0 @@
-class AddPmemberIdToProjectmemberships < ActiveRecord::Migration
-  def change
-    add_column :projectmemberships, :pmember_id, :integer
-  end
-end
