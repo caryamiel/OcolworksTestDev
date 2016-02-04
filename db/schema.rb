44b7723214ef6481fc758e6ba6446b560cf7506c
caryamiel <amielasparen@gmail.com>
2016-02-04 23:40:37 +0800

    add task_confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204133601) do
+ActiveRecord::Schema.define(version: 20160204153840) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -129,11 +129,12 @@ ActiveRecord::Schema.define(version: 20160204133601) do
     t.text     "descriptions"
     t.date     "startDate"
     t.date     "dueDate"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.integer  "project_id"
     t.integer  "user_id"
     t.string   "task_priority"
+    t.string   "task_confirmation"
   end
 
   create_table "users", force: :cascade do |t|
